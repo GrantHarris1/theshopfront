fb1e070b29bd115fb2a2697e4d683d37914a5c54
grantharriselectric <grantharriselectric@gmail.com>
2021-11-30 13:43:26 -0500

    log in

--- src/App.js
+++ src/App.js
@@ -13,19 +13,22 @@ import Tools from './pages/Tools';
 import Checkout from './pages/Checkout';
 import Return from './pages/Return';
 import Materials from './pages/Materials';
+import SignIn from './pages/SignIn';
 
 function App() {
     return (
         <Router>
             <Headline />
             <Routes>
-                <Route path="/home" element={<Home />} />
-                <Route path="/tools" element={<Tools />} />
-                <Route path="/checkout" element={<Checkout />} />
-                <Route path="/return" element={<Return />} />
-                <Route path="/materials" element={<Materials />} />
-
-
+                <Route path="/" >
+                    <Route path="tools" element={<Tools />} />
+                    <Route path="checkout" element={<Checkout />} />
+                    <Route path="return" element={<Return />} />
+                    <Route path="materials" element={<Materials />} />
+                    <Route path="signIn" element={<SignIn />} />
+                    <Route index element={<Home />} />
+                    <Route path="home" element={<Home />} />
+                </Route>
             </Routes>
         </Router>
     );
---
+++ src/pages/SignIn.js
@@ -0,0 +1,82 @@
+import React, { useState } from 'react'
+import { Card, Button, Col, Row, Container, InputGroup, FormControl } from 'react-bootstrap';
+import axios from 'axios';
+
+export default function SignIn() {
+    const [inputs, setInputs] = useState({});
+    const handleChange = e => {
+        setInputs(prevState => {
+            return {
+                ...prevState,
+                [e.target.name]: e.target.value
+            }
+        })
+    }
+    const handleSubmit = e => {
+        const data = {
+            grant_type: 'password',
+            client_id: 2,
+            client_secret: 'dnrWrQupuOAweX2339Nwaa2URfDK3CFrzcBbAvVR',
+            username: inputs.username,
+            password: inputs.password,
+            scope: ''
+        }
+        const headers = {
+            Accept: 'application/json',
+
+        }
+        axios({
+            method: 'post',
+            url: 'https://the_shop_backend-grantharriselectric334935.codeanyapp.com/oauth/token',
+            data,
+            headers
+        }).then(function (response) {
+            console.log(response);
+        });;
+    }
+    return (
+        <div className='SI'>
+            <br />
+            <Container className='Con text-center'>
+                <Row md={2} className="justify-content-center align-items-center">
+                    <Col>
+                        <Card>
+                            <Card.Header>Sign In To Request Tools or Materials</Card.Header>
+                            <Card.Body className='cBody' >
+                                <Card.Title>Use Employee Email to Log In.</Card.Title>
+                                <InputGroup className="mb-3">
+                                    <InputGroup.Text id="inputGroup-sizing-default">Email</InputGroup.Text>
+                                    <FormControl
+                                        name='username'
+                                        value={inputs.username || ''}
+                                        onChange={handleChange}
+                                        aria-label="Default"
+                                        aria-describedby="inputGroup-sizing-default"
+                                    />
+                                </InputGroup>
+
+                                <InputGroup className="mb-3">
+                                    <InputGroup.Text id="inputGroup-sizing-default">Password</InputGroup.Text>
+                                    <FormControl
+                                        name='password'
+                                        value={inputs.password || ''}
+                                        onChange={handleChange}
+                                        aria-label="Default"
+                                        aria-describedby="inputGroup-sizing-default"
+                                        type='password'
+                                    />
+                                </InputGroup>
+                                <Button variant="light"
+                                    onClick={handleSubmit}
+                                >Click Here To Sign In</Button>{''}
+                            </Card.Body>
+                            <Card.Footer className="text-muted">Thank you.</Card.Footer>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+
+
+        </div>
+    )
+}

2702be1d4347d98046d4a5df823af7e6879d1913
grantharriselectric <grantharriselectric@gmail.com>
2021-12-01 13:46:56 -0500

    tool table

--- src/pages/Tools.js
+++ src/pages/Tools.js
@@ -36,27 +36,6 @@ export default function Tools(props) {
                                 {mappedTools}
                             </tbody>
                         </Table>
-                        {/* <Card md='2'>
-                            <Card.Img variant="top" src = './src/style/img/Tools.png'/> 
-                            <Card.Body className='cBody text-light'>
-                                <Card.Title><h1>Tools</h1></Card.Title>
-                                <Card.Text>
-                                    <h2>Benders</h2>
-                                    <p>Filler</p>
-                                    <br />
-                                    <h2>Saws</h2>
-                                    <p>Lorem</p>
-                                    <br />
-                                    <h2>Drills</h2>
-                                    <p>Lorem</p>
-                                    <br />
-                                    <h2>Ladders</h2>
-                                    <p>Lorem</p>
-                                    <br />
-                                </Card.Text>
-                                <Button as={Link} to='/checkout' variant="light">Go to Checkout</Button>
-                            </Card.Body>
-                        </Card> */}
                     </Col>
                 </Row>
             </Container>

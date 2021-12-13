7fc5907f5a4db756ff121e1594a1816d580f70ea
grantharriselectric <grantharriselectric@gmail.com>
2021-12-12 22:35:59 -0500

    Some styiling updates

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Container, Row, Col} from 'react-bootstrap';
 
 
 
@@ -8,6 +9,13 @@ export default function Home() {
     return (
         <div>
             <br />
+            <Container>
+                <Row>
+                    <Col md="auto">
+                    <iframe src="https://open.spotify.com/embed/playlist/1YR6zbops5HskEqxl6E3uo?utm_source=generator&theme=0" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+            </Col>
+            </Row>
+            </Container>
             
             <br />
         </div>
